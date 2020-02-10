086d6be83fe18fcef65044a17698bbb3dd1d5948
Sarah Böning <Sarah.boening@tu-clausthal.de>
2020-02-10 14:40:35 +0100

    fix for split

--- splitcorp.py
+++ splitcorp.py
@@ -15,21 +15,22 @@ def get_paths(lang, path):
 def save_files(data_set, output_path, part):
 
     for file in data_set:
-        #name = file.split("\\")[-1]
-        name = file.split("/")[-1]
-        #file_path = os.path.join(output_path+"\\"+part+"\\", name)
-        file_path = os.path.join(output_path+"/"+part+"/", name)
-        output_file = open(file_path, "w")
-        with open(file, "r") as f:
+        print("wrinting file " + file)
+        name = file.split("\\")[-1]
+        #name = file.split("/")[-1]
+        file_path = os.path.join(output_path+"\\"+part+"\\", name)
+        #file_path = os.path.join(output_path+"/"+part+"/", name)
+        output_file = open(file_path, "w", encoding="UTF-8")
+        with open(file, "r", encoding="UTF-8", errors='ignore') as f:
             text = f.read()
         output_file.write(text)
         output_file.close()
 
 if __name__ == '__main__':
-    #path_to_java_corpus = "E:\\Hiwi\\BERT_undCo\\java_projects.tar\\java_projects\\java_projects\\"
-    #output_folder = "E:\\Hiwi\\BERT_undCo\\Split_Java_Corpus\\"
-    path_to_java_corpus = "/home/nilo4793/media/java_projects/"
-    output_folder = "/home/nilo4793/media/Split_Corpus/"
+    path_to_java_corpus = "E:\\Hiwi\\BERT_undCo\\java_projects.tar\\java_projects\\java_projects\\"
+    output_folder = "E:\\Hiwi\\BERT_undCo\\Split_Java_Corpus\\"
+    #path_to_java_corpus = "/home/nilo4793/media/java_projects"
+    #output_folder = "/home/nilo4793/media/Split_Corpus"
 
     file_list = get_paths('java', path_to_java_corpus)
     random.seed(666)
