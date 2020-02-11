ae50761d250fad812efd1f63cfcbe9e37bf46b35
Sarah Böning <Sarah.boening@tu-clausthal.de>
2020-02-11 18:23:55 +0100

    optimized splitcorp

--- splitcorp.py
+++ splitcorp.py
@@ -2,6 +2,7 @@ import os
 import random
 import glob
 
+
 def get_paths(lang, path):
     """
     :param lang: programming language aka file ending, e.g. 'java', 'py'
@@ -12,32 +13,42 @@ def get_paths(lang, path):
     paths = [f for f in glob.glob(path + '**/*.' + lang, recursive=True)]
     return paths
 
-def save_files(data_set, output_path, part):
 
+def save_files(data_set, output_path, part):
+    count = 1
     for file in data_set:
-        print("wrinting file " + file)
-        name = file.split("\\")[-1]
-        #name = file.split("/")[-1]
-        file_path = os.path.join(output_path+"\\"+part+"\\", name)
-        #file_path = os.path.join(output_path+"/"+part+"/", name)
-        output_file = open(file_path, "w", encoding="UTF-8")
+        #name = file.split("\\")[-1]
+        name = file.split("/")[-1]
+        #file_path = os.path.join(output_path+"\\"+part+"\\", name)
+        file_path = os.path.join(output_path+"/"+part+"/", name)
+        if os.path.isfile(file_path):
+            #file_path = os.path.join(output_path+"\\"+part+"\\", count + name)
+            file_path = os.path.join(output_path+"/"+part+"/", count + name)
+            count += 1
+        output_file = open(file_path, "w")
         try:
-            with open(file, "r", encoding="UTF-8", errors='ignore') as f:
+            with open(file, "r", encoding="UTF-8", errors='replace') as f:
                 text = f.read()
-            output_file.write(text)
-            output_file.close()
+                output_file.write(text)
+                output_file.close()
         except Exception as e:
-            print("Error processing file " + file)
+            print("Error processing file " + file )
+
 
 if __name__ == '__main__':
-    path_to_java_corpus = "E:\\Hiwi\\BERT_undCo\\java_projects.tar\\java_projects\\java_projects\\"
-    output_folder = "E:\\Hiwi\\BERT_undCo\\Split_Java_Corpus\\"
-    #path_to_java_corpus = "/home/nilo4793/media/java_projects"
-    #output_folder = "/home/nilo4793/media/Split_Corpus"
+    #path_to_java_corpus = "E:\\Hiwi\\BERT_undCo\\java_projects.tar\\java_projects\\java_projects\\"
+    #output_folder = "E:\\Hiwi\\BERT_undCo\\Split_Java_Corpus\\"
+    path_to_java_corpus = "/home/nilo4793/media/java_projects/"
+    output_folder = "/home/nilo4793/media/Split_Corpus/"
 
     file_list = get_paths('java', path_to_java_corpus)
     random.seed(666)
     random.shuffle(file_list)
+    for fi in file_list:
+        if os.path.isdir(fi):
+            file_list.remove(fi)
+            print("removed directory " + fi)
+
     thresh = int(len(file_list)/100*80)
     train_set = file_list[:thresh]
     eval_set = file_list[thresh+1:]
